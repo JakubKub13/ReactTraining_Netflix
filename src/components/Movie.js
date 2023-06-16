9d8166501dc8e11fc46e0d5d66fad9ba22a8b31f
JakubKub13 <jakubkubala3@gmail.com>
2023-06-16 20:51:57 +0200

    Delete one movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,20 +1,32 @@
 import "./Movie.css";
 import data from "../data";
+import MovieDeleteButton from "./MovieDeleteButton";
+import { useState } from "react";
 
 const Movie = () => {
+    const [movieList, setMovieList] = useState(data);
+
+    const deleteOneMovie = (id) => {
+        const filteredMovies = movieList.filter((oneMovie) => {
+            return oneMovie.id !== id;
+        })
+        setMovieList(filteredMovies)
+    }
+
     return (   
-        <div>
+        <div className="all-movies">
             {
-                data.map((oneMovie) => {
+                movieList.map((oneMovie) => {
                     const {id, image, title, age, tags, description} = oneMovie;
 
                     return (    
-                        <div key={id}>
+                        <div className="one-movie" key={id}>
                             <img src={image} alt="" />
                             <h2>{title}</h2>
                             <p>{age}</p>
                             <p>{tags}</p>
                             <p>{description}</p>
+                            <MovieDeleteButton deleteMovie={() => deleteOneMovie(id)}/>
                         </div>
                     )
                 })
